cb1e65d446bf4c471f4c4b0285daca035e2ed865
Henry Rodrick <moshisushi@gmail.com>
2018-05-21 23:00:56 +0100

    Celery config

--- vm_setup.sh
+++ vm_setup.sh
@@ -7,10 +7,16 @@ export DEBIAN_FRONTEND=noninteractive
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install vim
-sudo apt-get -y install postgresql
+sudo apt-get -y install redis-server
+sudo apt-get -y install celeryd
 sudo apt-get -y install gunicorn3
 sudo apt-get -y install ipython3
 sudo apt-get -y install python3-flask
 sudo apt-get -y install python3-psycopg2
+sudo apt-get -y install python3-redis
+sudo apt-get -y install python3-celery
 sudo apt-get -y install python3-sqlalchemy
-sudo -u postgres createdb reconlive
+
+sudo cp /workspace/celeryd /etc/default/celeryd
+
+sudo systemctl restart celeryd
--- webapp/app.py
+++ webapp/app.py
@@ -1,20 +1,34 @@
-import flask
+from flask import Flask, flash, request, redirect, url_for, send_file
+from werkzeug.utils import secure_filename
 from logger import logger
+import os.path
 
 import logging
 
-application = flask.Flask(__name__, static_url_path='')
+
+UPLOAD_FOLDER = '/var/lib/khal-remove/uploads'
+app = Flask(__name__, static_url_path='')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 for handler in logging.getLogger('gunicorn.error').handlers:
-    application.logger.addHandler(handler)
+    app.logger.addHandler(handler)
 
-@application.route('/')
+@app.route('/')
 def index():
-    return flask.send_file('static/index.html')
+    return send_file('static/index.html')
 
-@application.route('/api/v1/hello')
-def hello():
-    return "Hello"
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    filename = secure_filename(file.filename)
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return ""
 
 if __name__ == "__main__":
-    application.run(host='127.0.0.1', port=9999, debug=True)
+    app.run(host='127.0.0.1', port=9999, debug=True)
